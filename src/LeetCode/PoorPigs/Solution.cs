5327afc5a9cecaa7203174f52662fa71f41a28ef
layliu <layliu@microsoft.com>
2017-02-06 21:02:22 +0800

    Poor Pigs

---
+++ src/LeetCode/PoorPigs/Solution.cs
@@ -0,0 +1,24 @@
+﻿
+namespace LeetCode.PoorPigs
+{
+    public class Solution
+    {
+        public int PoorPigs(int buckets, int minutesToDie, int minutesToTest)
+        {
+            if (buckets <= 1)
+                return 0;
+
+            var NumberPerDim = minutesToTest / minutesToDie + 1;
+            var currentCount = 1;
+            int testedNumber = NumberPerDim;
+
+            while (testedNumber < buckets)
+            {
+                currentCount++;
+                testedNumber *= NumberPerDim;
+            }
+
+            return currentCount;
+        }
+    }
+}
